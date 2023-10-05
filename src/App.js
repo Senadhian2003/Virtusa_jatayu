9adc31dfec4a1bc1fa3c80e9869e18ec77e72042
Senthamil2003 <77163996+Senthamil2003@users.noreply.github.com>
2023-10-05 11:36:54 +0530

    sample commit

--- src/App.js
+++ src/App.js
@@ -1,63 +1,20 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Events from "./Components/Explore/events";
-import EServices from "./Components/Explore/eServices";
-import Media from "./Components/Explore/media";
-import Eventdetail from "./Components/Explore/event-detail";
-import Profile from "./Components/profile/profile";
-import StaffProfile from "./Components/profile/staff-profile";
 import Homepage from "./Components/Homepage/homepage";
-import Articles from "./Components/Explore/articles";
-import ArticleInfo from "./Components/Explore/articleInfo";
-import SuggestForm from "./Components/Explore/suggestion";
-import OrganizationRegister from "./Components/Homepage/organizationRegister";
-import EventDetail from "./Components/Explore/event-detail";
+import Login from "./Components/Authenticate/Login"
+import Signup from "./Components/Authenticate/Signup"
+import Admin from "./Components/Admin/Admin";
 
 function App() {
     return (
         <>
             <Router>
                 <Routes>
-                    <Route path="/" element={<Homepage />} />
-                    <Route path="/events" element={<Events />} />
-                    <Route path="/e_services" element={<EServices />} />
-                 
-                    <Route path="/explore/media" element={<Media />} />
-                    <Route
-                        path="/explore/event-detail"
-                        element={<Eventdetail />}
-                    />
-                    <Route path="/profile/profile" element={<Profile />} />
-           
-                    <Route
-                        path="/explore/profile/staffProfile"
-                        element={<StaffProfile />}
-                    />
-                    <Route
-                        path="/explore/profile/profile"
-                        element={<Profile />}
-                    />
-                    <Route
-                        path="/articles"
-                        element={<Articles />}
-                    />
-                    <Route
-                        path="/articles/:articleName"
-                        element={<ArticleInfo />}
-                    />
+                    <Route path="/home" element={<Homepage />} />
+                    <Route path="/" element={<Login/>} />
+                    <Route path="/signup" element={<Signup/>} />
+                    <Route path="/new" element={<Admin/>} />
 
-                    <Route
-                        path="explore/suggest_form"
-                        element={<SuggestForm />}
-                    />
-                    <Route
-                        path="/register"
-                        element={<OrganizationRegister />}
-                    />
-                    <Route
-                        path="/searchQuery"
-                        element={<EventDetail />}
-                    />
 
                 </Routes>
             </Router>
